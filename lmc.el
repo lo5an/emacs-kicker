cabf962fded2978967afad4fd5e6ad162b32b9d6
Logan Cox <lmc@ou.edu>
2016-04-05 12:42:20 -0500

    disable drupal stuff, seems slow? need to investigate

--- lmc.el
+++ lmc.el
@@ -37,29 +37,29 @@
 
 
 
-;; DRUPAL STUFF
-;; extend the php-mode in Emacs to follow Drupal coding style
-;; http://drupal.org/node/59868
-(defun drupal-mode ()
-  "Drupal php-mode."
-  (interactive)
-  (php-mode)
-  (message "Drupal mode activated.")
-  (set 'tab-width 2)
-  (set 'c-basic-offset 2)
-  (set 'indent-tabs-mode nil)
-  (c-set-offset 'case-label '+)
-  (c-set-offset 'arglist-intro '+) ; for FAPI arrays and DBTNG
-  (c-set-offset 'arglist-cont-nonempty 'c-lineup-math) ; for DBTNG fields and values
-  ; More Drupal-specific customizations here
-)
-(defun setup-php-drupal ()
-  ; Drupal
-  (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
-  (add-to-list 'auto-mode-alist '("/drupal.*\\.\\(php\\|inc\\)$" . drupal-mode))
-  (add-to-list 'auto-mode-alist '("\\.info" . conf-windows-mode))
-)
-(setup-php-drupal)
+;; ;; DRUPAL STUFF
+;; ;; extend the php-mode in Emacs to follow Drupal coding style
+;; ;; http://drupal.org/node/59868
+;; (defun drupal-mode ()
+;;   "Drupal php-mode."
+;;   (interactive)
+;;   (php-mode)
+;;   (message "Drupal mode activated.")
+;;   (set 'tab-width 2)
+;;   (set 'c-basic-offset 2)
+;;   (set 'indent-tabs-mode nil)
+;;   (c-set-offset 'case-label '+)
+;;   (c-set-offset 'arglist-intro '+) ; for FAPI arrays and DBTNG
+;;   (c-set-offset 'arglist-cont-nonempty 'c-lineup-math) ; for DBTNG fields and values
+;;   ; More Drupal-specific customizations here
+;; )
+;; (defun setup-php-drupal ()
+;;   ; Drupal
+;;   (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
+;;   (add-to-list 'auto-mode-alist '("/drupal.*\\.\\(php\\|inc\\)$" . drupal-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.info" . conf-windows-mode))
+;; )
+;; (setup-php-drupal)
 
 
 ; basic tab behavior 
